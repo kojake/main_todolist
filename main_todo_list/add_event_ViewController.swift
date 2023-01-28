04bfa3c98b6bdade7bdd285e9c4080202ef9191a
Yuki Takatsu <46854753+yuuki00682200@users.noreply.github.com>
2023-01-28 11:18:46 +0900

    Update add_event_ViewController.swift

--- main_todo_list/add_event_ViewController.swift
+++ main_todo_list/add_event_ViewController.swift
@@ -33,13 +33,16 @@ class add_event_ViewController: UIViewController {
     @IBAction func add_button(_ sender: Any) {
         let add_event = String(event_textfiled.text!)
         todo_list.append(add_event)
-        UserDefaults.standard.set(add_event, forKey: "todo_list_key")
-        
+        UserDefaults.standard.set(todo_list, forKey: "todo_list_key")
+        UserDefaults.standard.synchronize()
+        print("保存が成功しました。")
+
         //追加されたアラートを表示する
         let alert = UIAlertController(title: "追加", message: ("入力された" + add_event + "は追加されました"), preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
         
         self.present(alert, animated: true, completion: nil)
+        
     }
     
     //キーボードを閉じる
